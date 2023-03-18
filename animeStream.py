d52492bb3c29b2d87d88a5ffb52cfa9526277075
Merve Makbule Semizer <79668661+MerveMakbuleSemizer@users.noreply.github.com>
2023-03-18 17:51:45 +0300

    Add files via upload

---
+++ animeStream.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="Anime Recommender",
+    page_icon="https://www.asialogy.com/wp-content/uploads/anime-nedir-nasil-yapilir.jpg"
+    )
+st.title("Covid Survive Classifier Project")
+
+st.markdown("This website was created to predict whether a patient will succumb to Covid-19 disease based on some current symptoms and medical history. The model was trained by the dataset provided by the Mexican government.")
+st.image("https://www.asialogy.com/wp-content/uploads/anime-nedir-nasil-yapilir.jpg")
+st.sidebar.markdown("**Choose** User ID")
+# anime = pd.read_csv("anime.csv")
+# title = st.sidebar.selectbox("Title",(sorted(anime.name,reverse=True)))
+User_ID = np.loadtxt('user_IDs.txt', dtype=int)
+userID = st.sidebar.selectbox("User ID",(sorted(User_ID)))
+
+import detect_similars
+recommendations = detect_similars.detect_similar_users(userID)
+
+count = 1
+for anime in recommendations.index:
+    st.markdown(str(count) + anime)
+    count += 1
---
+++ detect_similars.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import scipy.stats
+
+import seaborn as sns
+from sklearn.metrics.pairwise import cosine_similarity
+
+cols = list(pd.read_csv("anime_new.csv",nrows=1))
+anime = pd.read_csv("anime_new.csv",dtype={"anime_id": "int32"},usecols=[i for i in cols if ((i !="members") & (i!="Unnamed: 0"))])
+ratings = pd.read_csv("rating.csv")
+
+temp_ratings = ratings.copy()
+temp_ratings.drop(index=list(ratings[ratings.rating==-1].index),axis=0,inplace=True)
+
+rating_count = temp_ratings.groupby(["user_id"]).agg(number_of_ratings=("rating","count")).reset_index()
+
+df = pd.merge(anime,pd.merge(temp_ratings,rating_count,on="user_id"),on="anime_id")
+df_last = df[df.number_of_ratings>300]
+matrix_form = df_last.pivot_table(index= "user_id", columns= "name", values= "rating_y")
+
+# Matrisi normalize ediyorum. Bunu yapabilmek için ortalama puanı sıfıra çekerek önyargıları ortadan kaldırdım. Bunu yapmamın 
+# sebebi bazı kullanıcılar genel ortalamının altında değerlendirme yapma eğilimdedirler "tough raters". Rating değerlerini
+# normalize ederek herkesi aynı seviyeye çekmiş oluyorum.
+matrix_norm = matrix_form.subtract(matrix_form.mean(axis=1),axis="rows")
+matrix_norm.head()
+
+user_sim_corr = matrix_norm.T.corr()
+
+# hedef kullanıcımızla benzer davranış gösteren ilk  10 kullanıcımızı korelasyon değerlerine bakarak belirledim.
+def detect_similar_users(target_user):
+    n = 10
+    threshold = 0.4 
+    similar_users = user_sim_corr[user_sim_corr[target_user] >= threshold][1:] 
+    watched_by_user = matrix_norm[matrix_norm.index == target_user].dropna(axis=1,how="all")
+    watched_by_similar_user = matrix_norm[matrix_norm.index.isin(similar_users.index)].dropna(axis=1,how="all")
+    watched_by_similar_user.drop(watched_by_user.columns,axis=1, inplace=True, errors='ignore') # benzer kullanıcıların izleyip hedef kullanıcının izlemediği animelerin listesi
+    
+    #weighted average ile hedef kullanıcımızın izlemediği animelere vereceği puanı tahmin ediyorum. 
+    similarity_correlation = user_sim_corr[user_sim_corr.index.isin(watched_by_similar_user.index)][target_user] 
+    temp = watched_by_similar_user.copy()
+    temp = temp.apply(lambda x:x*similarity_correlation.values)
+    sim_sum = (temp*0).apply(lambda x:x+similarity_correlation.values)
+    sim_sum.fillna(0)
+    sim_sum = sim_sum.apply(np.sum, axis = 0)
+    temp.fillna(0)
+    temp = temp.apply(np.sum, axis=0)
+    result = temp/sim_sum
+    result = result+matrix_form.mean() # normalize ettiğim değerleri eskisine çeviriyorum.
+    return result.sort_values(ascending=False)[:n]
